f9028016a22df0e711ef9c13eaec1a4f7a419b9a
misam-cybersurf <misamraza@fahm-technologies.com>
2025-01-14 18:03:05 +0530

    add items changes

--- src/Components/ui/Themes/DropDown.tsx
+++ src/Components/ui/Themes/DropDown.tsx
@@ -14,14 +14,14 @@ const Dropdown = ({
   toggleOpen,
   errors,
 }: {
+  options?: any[];
   label?: string;
   errors?:any;
-  options: string[];
   selectedOption?: string;
   className?: string;
   shadowclassName?: string;
   widthchange?: string;
-  onSelect: (option: string) => void;
+  onSelect: (option: any) => void;
   isOpen: boolean;
   toggleOpen: () => void;
 }) => {
@@ -61,26 +61,25 @@ const Dropdown = ({
       </p>
       {/* Dropdown Button */}
       <button
-  type="button" // Prevent form submission
-  onClick={(e) => {
-    e.stopPropagation(); // Prevent dialog close
-    toggleOpen();
-  }}
-  className={`bg-[#ffffff] text-[#4B4B4B] ${
-    shadowclassName ? shadowclassName : "shadow"
-  } shadow px-3 below-md:h-[38px] h-[35px] w-full ${
-    widthchange || "below-md:w-[100%] below-lg:w-full"
-  } rounded flex items-center justify-between below-md:w-full text-[12px] border focus:outline-none`}
->
-  <span>{selectedOption || "Year"}</span>
-  <img
-    src="/images/dropdown1.svg"
-    className={`-mr-0.5 transition-transform duration-200 ${
-      isOpen ? "transform rotate-180" : ""
-    }`}
-  />
-</button>
-
+        type="button" // Prevent form submission
+        onClick={(e) => {
+          e.stopPropagation(); // Prevent dialog close
+          toggleOpen();
+        }}
+        className={`bg-[#ffffff] text-[#4B4B4B] ${
+          shadowclassName ? shadowclassName : "shadow"
+        } shadow px-3 below-md:h-[38px] h-[35px] w-full ${
+          widthchange || "below-md:w-[100%] below-lg:w-full"
+        } rounded flex items-center justify-between below-md:w-full text-[12px] border focus:outline-none`}
+      >
+        <span>{selectedOption}</span>
+        <img
+          src="/images/dropdown1.svg"
+          className={`-mr-0.5 transition-transform duration-200 ${
+            isOpen ? "transform rotate-180" : ""
+          }`}
+        />
+      </button>
 
       {/* Dropdown Menu */}
       {isOpen && (
@@ -90,9 +89,9 @@ const Dropdown = ({
           } shadow`}
           style={{ zIndex: 50 }}
         >
-          {options?.map((option, index) => (
+          {options && options.length > 0 && options?.map((option) => (
             <div
-              key={index}
+              key={option?.id}
               onClick={(e) => {
                 e.stopPropagation(); // Prevent dialog close
                 onSelect(option);
@@ -100,7 +99,7 @@ const Dropdown = ({
               // onClick={() => onSelect(option)}
               className="cursor-pointer px-4 py-2 hover:bg-gray-100 border-b last:border-none"
             >
-              {option}
+              {option?.name}
             </div>
           ))}
         </div>
