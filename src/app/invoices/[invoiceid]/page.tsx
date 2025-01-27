d70c3911c0ccb93217a2875d4c408c05a7def1d7
misam-cybersurf <misamraza@fahm-technologies.com>
2025-01-27 22:32:43 +0530

    implement invoice detail changes

--- src/app/invoices/[invoiceid]/page.tsx
+++ src/app/invoices/[invoiceid]/page.tsx
@@ -173,8 +173,6 @@ const InvoiceDetails = () => {
             </div>
           </div>
         </div>
-
-
       </div>
       {/*table content*/}
       <div className=' shadow-sm  border border-b border-[#E4E4EF] block below-md:hidden rounded-md '>
@@ -200,7 +198,7 @@ const InvoiceDetails = () => {
           <div className='max-h-[calc(100vh-420px)] overflow-x-auto overflow-y-auto' style={{ scrollbarWidth: "thin" }}>
             <table className='w-full  text-[14px] table-auto shadow rounded-md'>
               <tbody>
-                {data?.invoicedetail?.map((row:any, index :any) => (
+                {data?.invoicedetail?.map((row: any, index: any) => (
                   <tr key={index}
                     className={index % 2 === 0 ? 'bg-white' : 'bg-[#F3F3F6]'}>
                     <td className="px-4 py-1.5 w-[10%] text-left text-[#636363]">{row?.itemcode}</td>
@@ -212,26 +210,23 @@ const InvoiceDetails = () => {
                     <td className="py-1.5 px-4  w-[100px] text-center text-[#636363]">{row?.packsize}</td>
                     <td className="py-1.5 px-4  w-[120px] text-right text-[#636363]">{row?.invtvalue}</td>
                     <td className="py-1.5 px-4  w-[100px] text-right text-[#030202]">{row?.unitprice}</td>
-                    <td className="py-1.5 px-4  w-[100px] text-right text-[#636363] ">{row?.tax}</td>
+                    <td className="py-1.5 px-4  w-[100px] text-right text-[#636363]">{row?.tax}</td>
                     <td className="py-1.5 px-4  w-[100px] text-right text-[#636363]">{row?.total}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-
         </div>
       </div>
 
       {/*card content for Mobile View*/}
       <div className='hidden  below-md:block'>
-        {tableData?.map((items, index) => (
+        {data?.invoicedetail?.map((items: any, index: any) => (
           <React.Fragment key={index}>
-            <div className='flex justify-between w-full mb-4  '>
-
+            <div className='flex justify-between w-full mb-4'>
               {/* Right Panel - Totals */}
               <div className="shadow border rounded-lg w-full gap-3 px-4 flex flex-col py-4 ">
-
                 <div className='flex gap-12 px-1'>
                   <span className="text-black text-[14px] font-bold">{items?.itemCode}</span>
                   <span className="text-black text-[16px] font-bold">{items?.description}</span>
@@ -239,48 +234,45 @@ const InvoiceDetails = () => {
                 <hr className='w-full h-[1px] my-2' color='lightgrey' />
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Brand</span>
-                  <span className="text-[#636363] text-[14px]">$8,068.89</span>
+                  <span className="text-[#636363] text-[14px]">{items?.brand}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Category</span>
-                  <span className="text-[#636363] text-[14px]">$8,076.39</span>
+                  <span className="text-[#636363] text-[14px]">{items?.category}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Quantity</span>
-                  <span className="text-[#636363] text-[14px]">$1.52</span>
+                  <span className="text-[#636363] text-[14px]">{items?.quantity}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Units</span>
-                  <span className="text-[#636363] text-[14px]">$1.52</span>
+                  <span className="text-[#636363] text-[14px]">{items?.unit}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Pack Size</span>
-                  <span className="text-[#636363] text-[14px]">$0</span>
+                  <span className="text-[#636363] text-[14px]">{items?.packsize}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Invt Value</span>
-                  <span className="text-[#636363] text-[14px]">$0</span>
+                  <span className="text-[#636363] text-[14px]">${items?.invtvalue}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Unit Price</span>
-                  <span className="text-[#636363] text-[14px]">$0</span>
+                  <span className="text-[#636363] text-[14px]">${items?.unitprice}</span>
                 </div>
                 <div className="flex justify-between">
                   <span className="text-[#636363] text-[12px]">Tax</span>
-                  <span className="text-[#636363] text-[14px]">$0</span>
+                  <span className="text-[#636363] text-[14px]">${items?.tax}</span>
                 </div>
                 <div className="flex justify-between border-t pt-2 mt-2">
                   <span className="text-[#636363] text-[14px] font-semibold">Total</span>
-                  <span className="text-[#636363] text-[14px] font-semibold">$8,079.43</span>
+                  <span className="text-[#636363] text-[14px] font-semibold">${items?.total}</span>
                 </div>
-
               </div>
-
             </div>
           </React.Fragment>
         ))}
       </div>
-
     </main>
   )
 }
