11549530fef0ebb6ee3c34ea56d16bfdccd33ed8
sabirali92 <sabir.virani@fahm-technologies.com>
2025-03-11 14:34:43 +0530

    changes in sales kpi

--- src/app/sales-kpi/page.tsx
+++ src/app/sales-kpi/page.tsx
@@ -729,82 +729,79 @@ const enhancedItems = items?.map((item:any) => ({
             </div>
           </div>
       </div>
-        {/*
-<div className="w-full px-3 below-md:w-[100%] mt-8 bg-white below-md:ml-0 tablet:ml-0 tablet:mt-6 tablet:w-full below-md:mt-3 shadow-md rounded-md">
-  <div className="flex flex-col">
-    {/* Header Section * /}
-    <div className="flex flex-row justify-between px-6 pt-6 items-start">
-      {/* Sales Information * /}
-      <div className="w-[50%] flex justify-center items-center">
-        <p className="text-[#393E47]  font-bold text-[16px]">
-        Sales by Items
-        </p>
-        {/* <p className="text-[#0A0A0A] font-bold text-[24px] mt-1">
-          $ {totalExtPrice.toLocaleString()}
-        </p> * /}
-        {/* <div className="flex flex-row gap-2 bg-[#ECFDF5] p-1 w-20 rounded-sm mt-2 items-center">
-          <img
-            src="/images/direction.svg"
-            alt="Growth Icon"
-            className="w-4 h-4"
-          />
-          <p className="text-[rgba(55, 96, 102, 0.8)] text-[12px]">
-            11.2%
-          </p>
-        </div> * /}
-      </div>
+      <div className="w-full px-3 below-md:w-[100%] mt-8 bg-white below-md:ml-0 tablet:ml-0 tablet:mt-6 tablet:w-full below-md:mt-3 shadow-md rounded-md">
+              <div className="flex flex-col">
+                {/* Header Section */}
+                <div className="flex flex-row justify-between px-6 pt-6 items-start">
+                  {/* Sales Information */}
+                  <div className="w-[50%] flex justify-center items-center">
+                    <p className="text-[#393E47]  font-bold text-[16px]">
+                    Sales by Items
+                    </p>
+                    {/* <p className="text-[#0A0A0A] font-bold text-[24px] mt-1">
+                      $ {totalExtPrice.toLocaleString()}
+                    </p> */}
+                    {/* <div className="flex flex-row gap-2 bg-[#ECFDF5] p-1 w-20 rounded-sm mt-2 items-center">
+                      <img
+                        src="/images/direction.svg"
+                        alt="Growth Icon"
+                        className="w-4 h-4"
+                      />
+                      <p className="text-[rgba(55, 96, 102, 0.8)] text-[12px]">
+                        11.2%
+                      </p>
+                    </div> */}
+                  </div>
 
-      {/* Dropdown * /}
-      <div className="mb-6 relative z-[40] below-md:w-[100%] below-md:max-w-[35%]">
-        {/* <Dropdown
-          className="relative below-md:w-full rounded"
-          shadowclassName="shadow-none border border-gray-200"
-          options={Yearoptions}
-          selectedOption={selectedYearOption4}
-          onSelect={(option) => handleYearSelect(option, 4)}
-          isOpen={isYearOpen4}
-          toggleOpen={() => toggleYearDropdown(4)}
-          widthchange="below-lg:w-[130px] tablet:w-[130px]"
-        /> * /}
-      </div>
-    </div>
+                  {/* Dropdown */}
+                  <div className="mb-6 relative z-[40] below-md:w-[100%] below-md:max-w-[35%]">
+                    {/* <Dropdown
+                      className="relative below-md:w-full rounded"
+                      shadowclassName="shadow-none border border-gray-200"
+                      options={Yearoptions}
+                      selectedOption={selectedYearOption4}
+                      onSelect={(option) => handleYearSelect(option, 4)}
+                      isOpen={isYearOpen4}
+                      toggleOpen={() => toggleYearDropdown(4)}
+                      widthchange="below-lg:w-[130px] tablet:w-[130px]"
+                    /> */}
+                  </div>
+                </div>
 
-    {/* Pie Chart Section * /}
-    <div className=" flex w-full justify-between flex-row gap-4">
-      <YearlySalesGraph enhancedItems={enhancedItems} totalQty={totalQty}  totalExtPrice={totalExtPrice}/>
-  
-
-    <div className="w-full">
-      <div className="w-full px-6 py-6 below-md:pb-12 tablet:pb-12">
-        <ul>
-          {enhancedItems?.map((item :any, index :any) => (
-            <li
-              key={index}
-              className="flex px-[15%] items-center justify-between py-2"
-            >
-              {/* Color Circle * /}
-              <div className="flex items-center">
-                <span
-                  className="inline-block w-3 h-3 rounded-full mr-3"
-                  style={{ backgroundColor: item.color }}
-                ></span>
-                <span className="text-[#000000B2] text-[12px]">
-                  {item.itemname}
-                </span>
+                {/* Pie Chart Section */}
+                <div className=" flex w-full justify-between flex-row gap-4">
+                  <YearlySalesGraph enhancedItems={enhancedItems} totalQty={totalQty}  totalExtPrice={totalExtPrice}/>
+            
+                <div className="w-full">
+                  <div className="w-full px-6 py-6 below-md:pb-12 tablet:pb-12">
+                    <ul>
+                      {enhancedItems?.map((item :any, index :any) => (
+                        <li
+                          key={index}
+                          className="flex px-[15%] items-center justify-between py-2"
+                        >
+                          {/* Color Circle */}
+                          <div className="flex items-center">
+                            <span
+                              className="inline-block w-3 h-3 rounded-full mr-3"
+                              style={{ backgroundColor: item.color }}
+                            ></span>
+                            <span className="text-[#000000B2] text-[12px]">
+                              {item.itemname}
+                            </span>
+                          </div>
+                          {/* Value */}
+                          <span className="font-semibold text-[#0A0A0A] text-[14px]">
+                            ${(item.totalextprice).toFixed(2)}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                </div>
               </div>
-              {/* Value * /}
-              <span className="font-semibold text-[#0A0A0A] text-[14px]">
-                ${(item.totalextprice).toFixed(2)}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-    </div>
-  </div>
-</div>
-*/}
+            </div>
         </div>
       </div>
       <div className="below-lg:hidden flex justify-end fixed bottom-5 right-5">
