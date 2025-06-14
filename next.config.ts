35d7eb714a0b24445ab6effdfff072974f96f6cf
sabirali92 <sabir.virani@fahm-technologies.com>
2025-06-14 12:38:08 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -5,45 +5,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // Apply to all routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline';
-              connect-src 'self' https://dev-api.talentin.ai;
-              img-src 'self' data:;
-              style-src 'self' 'unsafe-inline';
-              font-src 'self';
-            `
-              .replace(/\s{2,}/g, " ")
-              .trim(),
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=(), fullscreen=(self)",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
